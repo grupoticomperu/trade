91d0b81adde407e3b5ef11774eb0343b0b302472
Michael <michael@ticomperu.com>
2025-01-16 13:12:31 -0500

    idiomas instalado se creo la carpeta lang

--- resources/views/admin/tablesshow.blade.php
+++ resources/views/admin/tablesshow.blade.php
@@ -1,66 +1,75 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="text-xl font-semibold leading-tight text-gray-800">
-            {{ __('Menu General') }}
+            {{ __('General Menu') }}
         </h2>
     </x-slot>
 
     <section class="">
         <div class="py-12">
-           {{--  <div class="mx-auto max-w-7xl sm:px-6 lg:px-8"> --}}
-                {{-- <div class="overflow-hidden bg-white shadow-xl sm:rounded-lg"> --}}
+            {{--  <div class="mx-auto max-w-7xl sm:px-6 lg:px-8"> --}}
+            {{-- <div class="overflow-hidden bg-white shadow-xl sm:rounded-lg"> --}}
 
-                        <div class="grid grid-cols-1 px-4 mx-auto mt-4 max-w-7xl sm:px-6 lg:px-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-x-6 gap-y-8">
+            <div
+                class="grid grid-cols-1 px-4 mx-auto mt-4 max-w-7xl sm:px-6 lg:px-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-x-6 gap-y-8">
 
-                                {{--  @can('Configuration View') --}}
-                                <article>
-                                    <figure>
-                                        <img class="object-cover w-full rounded-xl h-36" src="{{asset('img/configuraciones.jpg')}}" alt="Configuraciones">
-                                    </figure>
-                                    <header class="mt-2">
-                                        <h1 class="text-xl text-center text-gray-700"><a href="{{-- {{route('admin.company.edit', auth()->user()->employee->company->id )}} --}}">Configuraciones</a></h1>
-                                    </header>
-                                </article>
-                               {{--  @endcan  --}}
+                {{--  @can('Configuration View') --}}
+                <article>
+                    <figure>
+                        <img class="object-cover w-full rounded-xl h-36" src="{{ asset('img/configuraciones.jpg') }}"
+                            alt="Configuraciones">
+                    </figure>
+                    <header class="mt-2">
+                        <h1 class="text-xl text-center text-gray-700"><a href="{{-- {{route('admin.company.edit', auth()->user()->employee->company->id )}} --}}">Configuraciones</a>
+                        </h1>
+                    </header>
+                </article>
+                {{--  @endcan  --}}
 
-                               @can('User List')
-                                <article>
-                                    <figure>
-                                        <img class="object-cover w-full rounded-xl h-36" src="{{asset('img/usuarios.jpg')}}" alt="Lita de Usuarios">
-                                    </figure>
-                                    <header class="mt-2">
-                                        <h1 class="text-xl text-center text-gray-700"><a href=" {{ route('admin.users.index') }}">Usuarios</a></h1>
-                                    </header>
+                @can('User List')
+                    <article>
+                        <figure>
+                            <img class="object-cover w-full rounded-xl h-36" src="{{ asset('img/usuarios.jpg') }}"
+                                alt="Lita de Usuarios">
+                        </figure>
+                        <header class="mt-2">
+                            <h1 class="text-xl text-center text-gray-700"><a
+                                    href=" {{ route('admin.users.index') }}">Usuarios</a></h1>
+                        </header>
 
-                                </article>
-                               @endcan
-                               
-                                @can('Permission List')
-                                <article>
-                                    <figure>
-                                        <img class="object-cover w-full rounded-xl h-36" src="{{asset('img/permisos.jpg')}}" alt="">
-                                    </figure>
-                                    <header class="mt-2">
-                                        <h1 class="text-xl text-center text-gray-700"><a href="{{route('admin.permissions.list')}}">Permisos</a></h1>
-                                    </header>
+                    </article>
+                @endcan
 
-                                </article>
-                               @endcan
+                @can('Permission List')
+                    <article>
+                        <figure>
+                            <img class="object-cover w-full rounded-xl h-36" src="{{ asset('img/permisos.jpg') }}"
+                                alt="">
+                        </figure>
+                        <header class="mt-2">
+                            <h1 class="text-xl text-center text-gray-700"><a
+                                    href="{{ route('admin.permissions.list') }}">Permisos</a></h1>
+                        </header>
 
-                                @can('Role List') 
-                                <article>
-                                    <figure>
-                                        <img class="object-cover w-full rounded-xl h-36" src="{{asset('img/roles.jpg')}}" alt="">
-                                    </figure>
-                                    <header class="mt-2">
-                                        <h1 class="text-xl text-center text-gray-700"><a href="{{route('admin.roles.index')}}">Roles</a></h1>
-                                    </header>
+                    </article>
+                @endcan
 
-                                </article>
-                                @endcan 
+                @can('Role List')
+                    <article>
+                        <figure>
+                            <img class="object-cover w-full rounded-xl h-36" src="{{ asset('img/roles.jpg') }}"
+                                alt="">
+                        </figure>
+                        <header class="mt-2">
+                            <h1 class="text-xl text-center text-gray-700"><a
+                                    href="{{ route('admin.roles.index') }}">Roles</a></h1>
+                        </header>
+
+                    </article>
+                @endcan
 
 
-                                {{-- @can('Local View')
+                {{-- @can('Local View')
                                 <article>
                                     <figure>
                                         <img class="object-cover w-full rounded-xl h-36" src="{{asset('img/locals.jpg')}}" alt="">
@@ -170,8 +179,8 @@
 
 
 
-                        </div>
-                {{-- </div> --}}
+            </div>
+            {{-- </div> --}}
             {{-- </div> --}}
         </div>
 
